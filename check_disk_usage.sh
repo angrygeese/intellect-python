096d97aa228f3588efbf18447cb5566cf7936a47
angrygeese <d2707bredov@gmail.com>
2022-03-31 16:10:48 +0000

    add voice line

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,8 @@ else
   echo "$dir directory does not exist, exiting"
   exit 101
 fi
+echo -e "The heaviest subfolder of $dir is:"
+top1=$(du -S $dir | sort -rn | head -n 1 | cut -f2)
+disk=$(du -Sh -d 0 $top1 | sort -rn | head -n 1 | cut -f1)
+echo "$top1 with disk usage of $disk"
+echo "The heaviest subfolder of $dir is: $top1" | festival --tts"
