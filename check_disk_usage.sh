f55e6d3b7861d5ed681860eef6ed23bb32dd1031
angrygeese <d2707bredov@gmail.com>
2022-03-31 16:25:21 +0000

    fixed voice line error

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -10,4 +10,4 @@ echo -e "The heaviest subfolder of $dir is:"
 top1=$(du -S $dir | sort -rn | head -n 1 | cut -f2)
 disk=$(du -Sh -d 0 $top1 | sort -rn | head -n 1 | cut -f1)
 echo "$top1 with disk usage of $disk"
-echo "The heaviest subfolder of $dir is: $top1" | festival --tts"
+echo "The heaviest subfolder of $dir is: $top1" | festival --tts
